364109bd3c61f07e7a046e1f61479cc0f4039573
Bruno Batalha <brunobatalha37@gmail.com>
2022-04-09 17:59:54 -0400

    Create getUserByKey in UserPublicService

--- src/app/services/userPublic/user-public.service.ts
+++ src/app/services/userPublic/user-public.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
-import { Observable } from 'rxjs';
+import { map, Observable } from 'rxjs';
 import { User } from 'src/app/models/User';
 import { BaseService } from '../base.service';
 
@@ -8,13 +8,27 @@ import { BaseService } from '../base.service';
 	providedIn: 'root',
 })
 export class UserPublicService extends BaseService<User> {
+	private usersPublicPath = 'users/public';
+
 	constructor(database: AngularFireDatabase) {
 		super(database);
 	}
 
+	getUserByKey(key: string): Observable<User | null> {
+		return this.filterByKey(this.usersPublicPath, key).pipe(
+			map((users) => {
+				if (users.length > 0) {
+					return users[0];
+				} else {
+					return null;
+				}
+			})
+		);
+	}
+
 	filterByUsername(username: string): Observable<User | null> {
 		return new Observable<User | null>((subscriber) => {
-			this.filterBy('/users/public', 'username', username).subscribe((observer) => {
+			this.filterBy(this.usersPublicPath, 'username', username).subscribe((observer) => {
 				if (observer.length > 0) {
 					const user = observer[0].payload.val() as User;
 					subscriber.next({ key: observer[0].key, ...user } as User);
