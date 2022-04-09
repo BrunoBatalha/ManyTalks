6dee30c9c41190e2ac8167b1d6d56b69aff96c84
Bruno Batalha <brunobatalha37@gmail.com>
2022-04-09 17:59:36 -0400

    List talks and validate creation new talk

--- src/app/pages/talks/talks.component.ts
+++ src/app/pages/talks/talks.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { concatMap, map, of, throwError } from 'rxjs';
 import { User } from 'src/app/models/User';
 import { UserService } from 'src/app/services/user/user.service';
 import { UserPublicService } from 'src/app/services/userPublic/user-public.service';
 import { UserTalkService } from 'src/app/services/userTalk/user-talk.service';
-import { URL } from 'src/app/shared/constants';
+import { ERROR, URL } from 'src/app/shared/constants';
 
 @Component({
 	selector: 'app-talks',
@@ -15,7 +16,7 @@ export class TalksComponent implements OnInit {
 	urlChat: string = URL.CHAT;
 	usernameToTalk: string = '';
 	currentUser: User | null = null;
-	talks: string[] = [];
+	talkKeys: string[] = [];
 
 	constructor(
 		private userService: UserService,
@@ -31,45 +32,76 @@ export class TalksComponent implements OnInit {
 	}
 
 	loadUser(): void {
-		if (this.userService.isLogged()) {
-			this.currentUser = this.userService.getFromStorage();
+		const loggedUser = this.userService.getFromStorage();
+		if (loggedUser?.key == null) {
+			this.createUser();
 		} else {
-			const user: User = {
-				username: 'Batalha',
-				// username: new Date().getTime().toString(),
-			};
-			this.userService.insertUser(user).subscribe((user): void => {
-				const isSaved = this.userService.saveInStorage(user);
-				if (isSaved) {
-					this.currentUser = user;
+			this.userPublicService.getUserByKey(loggedUser.key).subscribe((user) => {
+				if (user != null) {
+					this.currentUser = loggedUser;
+				} else {
+					this.createUser();
 				}
 			});
 		}
 	}
 
-	createTalk(): void {
-		this.userPublicService.filterByUsername(this.usernameToTalk).subscribe((userFound: User | null) => {
-			if (userFound != null) {
-				this.userTalkService.getUserTalk(this.currentUser?.key, userFound.key).subscribe((_) => {
-					console.log(_);
-					if (_ === null) {
-						this.userTalkService.insertUserTalk(this.currentUser?.key, userFound.key).subscribe((z) => {
-							console.log(z);
-						});
-					}
-				});
-
-				// this.router.navigate(['chat']);
-			} else {
-				alert('Não encontrado');
+	createUser(): void {
+		const user: User = {
+			username: 'Batalha',
+			// username: new Date().getTime().toString(),
+		};
+		this.userService.insertUser(user).subscribe((user): void => {
+			const isSaved = this.userService.saveInStorage(user);
+			if (isSaved) {
+				this.currentUser = user;
 			}
 		});
 	}
 
+	createTalk(): void {
+		this.userPublicService
+			.filterByUsername(this.usernameToTalk)
+			.pipe(
+				concatMap((userFound: User | null) => {
+					if (userFound === null) {
+						return throwError(() => ERROR.NOT_FOUND_USER);
+					}
+
+					return this.userTalkService.getKeyByUserKeys(this.currentUser?.key, userFound.key).pipe(
+						map((userTalkKey) => ({
+							userTalkKey: userTalkKey,
+							userFound: userFound,
+						}))
+					);
+				}),
+				concatMap((resultGetUserTalk) => {
+					if (resultGetUserTalk?.userTalkKey !== null) {
+						return of(resultGetUserTalk.userTalkKey);
+					}
+					return this.userTalkService.insertUserTalk(this.currentUser?.key, resultGetUserTalk.userFound.key);
+				})
+			)
+			.subscribe({
+				next: (userTalkKey) => {
+					this.router.navigate([URL.CHAT, userTalkKey]);
+				},
+				error: (err) => {
+					if (err == ERROR.NOT_FOUND_USER) {
+						alert('Usuário não encontrado');
+					} else {
+						alert('error desconhecido: ' + err);
+					}
+				},
+			});
+	}
+
 	listTalks(): void {
-		this.userTalkService.filterUserTalksByKey(this.currentUser?.key).subscribe((o) => {
-			this.talks = o;
-		});
+		if (this.currentUser != null) {
+			this.userTalkService.filterByUserKey(this.currentUser?.key).subscribe((talkKeys) => {
+				this.talkKeys = talkKeys;
+			});
+		}
 	}
 
 	trackByTalks(index: number): number {
