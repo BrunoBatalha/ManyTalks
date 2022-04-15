a2528a9753c2a6d5fbe71df87643cbb97921011b
Bruno Batalha <brunobatalha37@gmail.com>
2022-04-15 18:21:21 -0400

    refactor: eslint

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContainerComponent } from './components/container/container.component';
+import { MessageBalloonComponent } from './components/message-balloon/message-balloon.component';
 import { TalkListComponent } from './components/talk-list/talk-list.component';
 import { TalkRowComponent } from './components/talk-row/talk-row.component';
 import { ChatComponent } from './pages/chat/chat.component';
@@ -16,15 +17,23 @@ import { TalksComponent } from './pages/talks/talks.component';
 import { appReducer } from './store/app.state';
 
 @NgModule({
-	declarations: [AppComponent, ChatComponent, TalksComponent, ContainerComponent, TalkRowComponent, TalkListComponent],
+	declarations: [
+		AppComponent,
+		ChatComponent,
+		TalksComponent,
+		ContainerComponent,
+		TalkRowComponent,
+		TalkListComponent,
+		MessageBalloonComponent,
+	],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
 		FormsModule,
 		AngularFireModule.initializeApp(environment.firebase),
 		AngularFireDatabaseModule,
-		StoreModule.forRoot({app:appReducer}, {}),
-		StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
+		StoreModule.forRoot({ app: appReducer }, {}),
+		StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
 	],
 	providers: [],
 	bootstrap: [AppComponent],
