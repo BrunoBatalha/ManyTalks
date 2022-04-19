bd738b281680fb7294fe7af0ead366cc59c4c9bf
Bruno Batalha <brunobatalha37@gmail.com>
2022-04-18 22:37:46 -0400

    env: generate env only production

--- scripts/set-env.ts
+++ scripts/set-env.ts
@@ -7,28 +7,30 @@ require('dotenv').config();
 // Configure Angular `environment.ts` file path
 const environment = argv.environment;
 const isProduction = environment === 'prod';
-const targetPath = isProduction ? `./src/environments/environment.prod.ts` : `./src/environments/environment.ts`;
+if (isProduction) {
+	const targetPath = isProduction ? `./src/environments/environment.prod.ts` : `./src/environments/environment.ts`;
 
-// `environment.ts` file structure
-const envConfigFile = `export const environment = {
-    production: ${process.env['PRODUCTION']},
-	firebase: {
-		apiKey: '${process.env['API_KEY']}',
-		authDomain: '${process.env['AUTH_DOMAIN']}',
-		databaseURL: '${process.env['DATABASE_URL']}',
-		projectId: '${process.env['PROJECT_ID']}',
-		appId: '${process.env['APP_ID']}',
-	},
-};
-`;
-console.log('==================================================================== \n');
-console.log('The file `environment.ts` will be written with the following content: \n');
-// console.log(colors.grey(envConfigFile));
-writeFile(targetPath, envConfigFile, function (err: any) {
-	if (err) {
-		throw console.error(err);
-	} else {
-		console.log(`Angular environment.ts file generated correctly at ${targetPath} \n`);
-	}
+	// `environment.ts` file structure
+	const envConfigFile = `export const environment = {
+		production: ${process.env['PRODUCTION']},
+		firebase: {
+			apiKey: '${process.env['API_KEY']}',
+			authDomain: '${process.env['AUTH_DOMAIN']}',
+			databaseURL: '${process.env['DATABASE_URL']}',
+			projectId: '${process.env['PROJECT_ID']}',
+			appId: '${process.env['APP_ID']}',
+		},
+	};
+	`;
 	console.log('==================================================================== \n');
-});
+	console.log('The file `environment.ts` will be written with the following content: \n');
+	// console.log(colors.grey(envConfigFile));
+	writeFile(targetPath, envConfigFile, function (err: any) {
+		if (err) {
+			throw console.error(err);
+		} else {
+			console.log(`Angular environment.ts file generated correctly at ${targetPath} \n`);
+		}
+		console.log('==================================================================== \n');
+	});
+}
