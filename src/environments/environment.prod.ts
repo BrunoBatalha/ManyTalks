ec49180f50742bee88da9538ce66c8c8a3324286
Bruno Batalha <brunobatalha37@gmail.com>
2022-04-18 22:14:21 -0400

    ci: adjust script set-env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,10 @@
 export const environment = {
-	production: true,
-	firebase: process.env['FIREBASE'],
+    production: false,
+	firebase: {
+		apiKey: 'AIzaSyBhd4bmgwQKE_CeqvHHVxrVaSw0Unyt64o',
+		authDomain: 'manytalks.firebaseapp.com',
+		databaseURL: 'https://manytalks-default-rtdb.firebaseio.com',
+		projectId: 'manytalks',
+		appId: '1:126254722022:web:3923876f84434bbadd0427',
+	},
 };
