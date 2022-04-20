572c36c4f05cf4203239a7f3697838882f0ac15e
Bruno Batalha <brunobatalha37@gmail.com>
2022-04-20 19:22:02 -0400

    style: format

--- src/app/components/talk-list/talk-list.component.ts
+++ src/app/components/talk-list/talk-list.component.ts
@@ -6,33 +6,35 @@ import { UserTalkService } from 'src/app/services/userTalk/user-talk.service';
 import { IAppState } from 'src/app/store/app.state';
 
 @Component({
-  selector: 'app-talk-list',
-  templateUrl: './talk-list.component.html',
-  styleUrls: ['./talk-list.component.css'],
+	selector: 'app-talk-list',
+	templateUrl: './talk-list.component.html',
+	styleUrls: ['./talk-list.component.css'],
 })
 export class TalkListComponent implements OnInit {
-  talkKeys: string[] = [];
+	talkKeys: string[] = [];
 
-  constructor(private store: Store<{ app: IAppState }>, private userTalkService: UserTalkService) { }
+	constructor(private store: Store<{ app: IAppState }>, private userTalkService: UserTalkService) {}
 
-  ngOnInit(): void {
-    this.store.select('app').pipe(map(s => s.user)).subscribe(user => {
-      if (user) {
-        this.listTalks(user);
-      }
-    });
+	ngOnInit(): void {
+		this.store
+			.select('app')
+			.pipe(map((s) => s.user))
+			.subscribe((user) => {
+				if (user) {
+					this.listTalks(user);
+				}
+			});
+	}
 
-  }
+	listTalks(user: User): void {
+		if (user != null) {
+			this.userTalkService.filterByUserKey(user.key).subscribe((talkKeys) => {
+				this.talkKeys = talkKeys;
+			});
+		}
+	}
 
-  listTalks(user: User): void {
-    if (user != null) {
-      this.userTalkService.filterByUserKey(user.key).subscribe((talkKeys) => {
-        this.talkKeys = talkKeys;
-      });
-    }
-  }
-
-  trackByTalks(index: number): number {
+	trackByTalks(index: number): number {
 		return index;
 	}
 }
